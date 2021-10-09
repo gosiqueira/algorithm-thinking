db4a979ec849338d679c099a567003a196ca8a22
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-10-08 22:23:37 -0300

    Fix code style to match the book

--- book/food-lines.c
+++ book/food-lines.c
@@ -5,7 +5,7 @@
 int shortest_line_index(int lines[], int n) {
     int j;
     int shortest = 0;
-    for (j = 1; j < n; ++j) 
+    for (j = 1; j < n; j++) 
         if (lines[j] < lines[shortest])
             shortest = j;
             
@@ -15,9 +15,10 @@ int shortest_line_index(int lines[], int n) {
 
 void solve(int lines[], int n, int m) {
     int i, shortest;
-    for (i = 0; i < m; ++i) {
+    for (i = 0; i < m; i++) {
         shortest = shortest_line_index(lines, n);
-        printf("%d\n", lines[shortest]++);
+        printf("%d\n", lines[shortest]);
+        lines[shortest]++;
     }
 }
 
@@ -26,7 +27,7 @@ int main(void) {
     int lines[MAX_LINES];
     int n, m, i;
     scanf("%d%d", &n, &m);
-    for (i = 0; i < n; ++i)
+    for (i = 0; i < n; i++)
         scanf("%d", &lines[i]);
     solve(lines, n, m);
     return 0;
